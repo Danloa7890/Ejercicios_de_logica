977b7d41ad6fd7ebdb4c51fa44933177c93ccd7e
Danloa7890 <daniloloaizagomezcuello@gmail.com>
2026-01-10 13:34:25 -0500

    Agrega ejercicio contar del 1 al 100

---
+++ iteration_master.py
@@ -0,0 +1,45 @@
+'''
+Quiero contar del 1 al 20 de uno en uno (imprimiendo cada uno).
+¿De cuántas maneras eres capaz de hacerlo?
+Crea el código para cada una de ellas.
+'''
+
+print("===== METODO 1: FOR =====")
+for i in range(1, 21):
+    print(i)
+
+print("\n===== METODO 2: WHILE =====")
+i = 1
+while i <= 20:
+    print(i)
+    i += 1
+
+print("\n===== METODO 3: RECURSIVIDAD =====")
+def contar(n):
+    if n > 20:
+        return
+    print(n)
+    contar(n + 1)
+
+contar(1)
+
+print("\n===== METODO 4: LISTA + FOR =====")
+numeros = list(range(1, 21))
+for n in numeros:
+    print(n)
+
+print("\n===== METODO 5: COMPRENSION DE LISTA =====")
+[print(i) for i in range(1, 21)]
+
+print("\n===== METODO 6: MAP =====")
+list(map(print, range(1, 21)))
+
+print("\n===== METODO 7: GENERADOR =====")
+def generador():
+    for i in range(1, 21):
+        yield i
+
+for numero in generador():
+    print(numero)
+
+
